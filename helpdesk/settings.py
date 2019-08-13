28e4fc437ce228ee98228f089384b2fee82d001b
Marek <power88w@gmail.com>
2019-08-13 01:58:28 +0100

    add require2

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool,default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['helpdeskappp.herokuapp.com']
 
 
 # Application definition
