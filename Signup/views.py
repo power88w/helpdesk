a626a9cf22c8a9c30aa3c3662435005b88af3941
Marek <power88w@gmail.com>
2019-08-13 02:38:56 +0100

    fixing email

--- Signup/views.py
+++ Signup/views.py
@@ -19,7 +19,7 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
             print(form)
-            user.is_active = True
+            user.is_active = False
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your blog account.'
@@ -40,7 +40,7 @@ def signup(request):
 
     else:
         form = SignupForm()
-    return redirect('/hardware/')
+    return render(request, 'registration/registration.html', {'form': form})
 
 
 def activate(request, uidb64, token):
