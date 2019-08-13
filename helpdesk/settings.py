5de91de0cf5a345c8d44831d3261da5398395a76
Marek <power88w@gmail.com>
2019-08-13 02:50:10 +0100

    fix registration

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool,default=True)
 
-ALLOWED_HOSTS = ['helpdeskappp.herokuapp.com']
+ALLOWED_HOSTS = ['helpdeskappp.herokuapp.com', '127.0.0.2']
 
 
 # Application definition
